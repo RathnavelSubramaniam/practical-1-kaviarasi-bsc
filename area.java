21978483f0d294aeeca4c780f8392381e389bfcb
kaviarasi-bsc <142780143+kaviarasi-bsc@users.noreply.github.com>
2023-08-22 07:19:22 +0000

    completed

--- area.java
+++ area.java
@@ -1,6 +1,6 @@
 class area{
     public static void main(String args[]){
-    double pi=3.14,r=3,a;
+    double pi=3.14,r=10.0,a;
      a = pi*r*r;
     System.out.println("The area of the circle with radius 10.0 is: "+a);
     }
